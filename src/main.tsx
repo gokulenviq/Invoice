b699d08f7781be3e8a181a383d7c168ef2833cd6
gokulenviq <gokul@enviqtech.com>
2024-04-14 23:52:12 -0700

    main

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './css/style.css';
 import './css/satoshi.css';
 
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
   </React.StrictMode>,
+  document.getElementById('root')
 );
