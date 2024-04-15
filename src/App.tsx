b3ca28eff3433759f1ea7f7440bf3f06676a7ffb
gokulenviq <gokul@enviqtech.com>
2024-04-14 23:33:30 -0700

    main code

--- src/App.tsx
+++ src/App.tsx
@@ -30,16 +30,8 @@ function App() {
       <Route path="/" element={ <><PageTitle title="Signin " /><SignIn /></> }/>
       <Route path="/home" element={  <><PageTitle title="Home " /><ECommerce /></>}/>
       <Route path="/invoiceform" element={  <><PageTitle title="Create Invoice" /><FormLayout /></>}/>
-     <Route  path="/tables"  element={ <><PageTitle title="Manage Invoice " /><Tables /></>}/> 
-      <Route path="/settings" element={  <><PageTitle title="Settings " /><Settings /></>}/>
-       
-        
-         
-     
-        
-     
-   
-       
+      <Route  path="/tables"  element={ <><PageTitle title="Manage Invoice " /><Tables /></>}/> 
+      <Route path="/settings" element={  <><PageTitle title="Settings " /><Settings /></>}/>       
       </Routes>
     </>
   );
